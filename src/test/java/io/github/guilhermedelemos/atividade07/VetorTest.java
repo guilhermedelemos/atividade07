302cf11bf223c72a3215e571614dacc1f437b3bd
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-05-15 00:28:27 -0300

    Melhoria

--- src/test/java/io/github/guilhermedelemos/atividade07/VetorTest.java
+++ src/test/java/io/github/guilhermedelemos/atividade07/VetorTest.java
@@ -7,6 +7,7 @@ package io.github.guilhermedelemos.atividade07;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import static org.mockito.Mockito.*;
 
 /**
@@ -15,22 +16,35 @@ import static org.mockito.Mockito.*;
  */
 public class VetorTest {
 
+    private static Vetor v;
+    private static ValidarVetor vv;
+    private static double[] vetorNulo, vetorVazio, vetorPar, vetorImpar;
+
     public VetorTest() {
+    }
+
+    @Before
+    public void setUp() {
+        v = new Vetor();
+        vv = new ValidarVetor();
+        vetorNulo = null;
+        double[] vazio = {};
+        vetorVazio = vazio;
+        double[] par = {5.8, 4.6, 9.2, 1.1};
+        vetorPar = par;
+        double[] impar = {5.8, 4.6, 9.2};
+        vetorImpar = impar;
     }
 
     @Test(expected = Exception.class)
     public void validarVetorNuloTest() throws Exception {
-        double[] entrada = null;
-        ValidarVetor vv = new ValidarVetor();
-        vv.validar(entrada);
+        vv.validar(vetorNulo);
     }
 
     @Test
     public void validarVetorVazioTest() throws Exception {
-        double[] entrada = {};
-        ValidarVetor vv = new ValidarVetor();
         try {
-            vv.validar(entrada);
+            vv.validar(vetorVazio);
         } catch (Exception e) {
             assertEquals("Vetor com zero elementos", e.getMessage());
         }
@@ -38,18 +52,12 @@ public class VetorTest {
 
     @Test
     public void medianaTest() {
-        Vetor v = new Vetor();
-
-        double[] entradaImpar = {5.8, 4.6, 9.2};
-        assertEquals(5.8, v.mediana(entradaImpar), 0.1);
-
-        double[] entradaPar = {5.8, 4.6, 9.2, 1.1};
-        assertEquals(5.2, v.mediana(entradaPar), 0.1);
+        assertEquals(5.8, v.mediana(vetorImpar), 0.1);
+        assertEquals(5.2, v.mediana(vetorPar), 0.1);
     }
 
     @Test
     public void mediaTest() {
-        Vetor v = new Vetor();
         double[] entrada = {5.8, 4.6, 9.2};
         assertEquals(6.53, v.media(entrada), 0.1);
     }

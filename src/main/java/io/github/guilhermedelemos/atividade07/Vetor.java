cbbd095b806bc85a73fd105ff00680dd2f6a6e3c
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-05-15 02:02:06 -0300

    Implementações N maiores/menores

--- src/main/java/io/github/guilhermedelemos/atividade07/Vetor.java
+++ src/main/java/io/github/guilhermedelemos/atividade07/Vetor.java
@@ -5,7 +5,10 @@
  */
 package io.github.guilhermedelemos.atividade07;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
@@ -31,57 +34,105 @@ public class Vetor {
         }
         return resultado / v.length;
     }
-    
+
     public Extremos getExtremos(double[] v) {
         Extremos e = new Extremos();
         e.setMenor(v[0]);
         e.setMaior(v[0]);
-        for(int i=0;i<v.length;i++) {
-            if(v[i] < e.getMenor()) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] < e.getMenor()) {
                 e.setMenor(v[i]);
                 e.setIndiceMenor(i);
             }
-            if(v[i] > e.getMaior()) {
+            if (v[i] > e.getMaior()) {
                 e.setMaior(v[i]);
                 e.setIndiceMaior(i);
             }
         }
         return e;
     }
-    
+
     public int getQtdeValoresAcimaMedia(double[] v) {
         double media = media(v);
         int contador = 0;
-        for(int i=0;i<v.length;i++) {
-            if(v[i] > media) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > media) {
                 contador++;
             }
         }
         return contador;
     }
-    
+
     public int getQtdeValoresAbaixoMedia(double[] v) {
         double media = media(v);
         int contador = 0;
-        for(int i=0;i<v.length;i++) {
-            if(v[i] < media) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] < media) {
                 contador++;
             }
         }
         return contador;
     }
-    
+
     public double variancia(double[] v) {
         double media = media(v);
         double soma = 0;
-        for(int i=0;i<v.length;i++) {
+        for (int i = 0; i < v.length; i++) {
             soma += Math.pow(v[i] - media, 2);
         }
         return soma / v.length;
     }
-    
+
     public double desvioPadrao(double[] v) {
         return Math.sqrt(variancia(v));
+    }
+
+    public double[] encontrarMaiores(double[] v, int qtde) {
+        double[] retorno = new double[qtde];
+        Arrays.fill(retorno, Integer.MIN_VALUE);
+        for (double e : v) {
+            if (e > retorno[qtde - 1]) {
+                retorno[qtde - 1] = e;
+                
+                Arrays.sort(retorno);
+                double[] apoio = new double[retorno.length];
+                int j = retorno.length-1;
+                for (int i = 0; i < apoio.length; i++) {
+                    apoio[i] = retorno[j];
+                    j--;
+                }
+                retorno = apoio;
+            }
+        }
+        return retorno;
+    }
+
+    public double[] encontrarMenores(double[] v, int qtde) {
+        /*List<Double> elementos = new ArrayList<>();
+        double menor = Double.MAX_VALUE;
+        for(int i=0;i<qtde;i++) {
+            for(int j=0;j<v.length;j++) {
+                if(v[j] < menor) {
+                    menor = v[j];
+                }
+            }
+            elementos.add(menor);
+        }
+        double[] retorno = new double[elementos.size()];
+        for(int i=0;i<retorno.length;i++) {
+            retorno[i] = elementos.get(i);
+        }
+        Arrays.sort(retorno);
+        return retorno;*/
+        double[] retorno = new double[qtde];
+        Arrays.fill(retorno, Integer.MAX_VALUE);
+        for (double e : v) {
+            if (e < retorno[qtde - 1]) {
+                retorno[qtde - 1] = e;
+                Arrays.sort(retorno);
+            }
+        }
+        return retorno;
     }
 
 }
